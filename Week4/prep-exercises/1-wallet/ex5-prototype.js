d942c047bed8592532343e3363dd14ed771c952a
Hana <150429887+hhana1-a@users.noreply.github.com>
2024-02-17 20:27:22 +0100

    update

--- Week4/prep-exercises/1-wallet/ex5-prototype.js
+++ Week4/prep-exercises/1-wallet/ex5-prototype.js
@@ -3,23 +3,32 @@ import eurosFormatter from './euroFormatter.js';
 function Wallet(name, cash) {
   this._name = name;
   this._cash = cash;
+  this._dailyAllowance = 40;
+  this._dayTotalWithdrawals = 0;
 }
 
-Wallet.prototype.deposit = function (amount) {
+Wallet.prototype.deposit = function(amount) {
   this._cash += amount;
+  this.checkResetDailyAllowance();
 };
 
-Wallet.prototype.withdraw = function (amount) {
+Wallet.prototype.withdraw = function(amount) {
   if (this._cash - amount < 0) {
     console.log(`Insufficient funds!`);
     return 0;
   }
 
+  if (this._dayTotalWithdrawals + amount > this._dailyAllowance) {
+    console.log(`Exceeds daily withdrawal limit!`);
+    return 0;
+  }
+
   this._cash -= amount;
+  this._dayTotalWithdrawals += amount;
   return amount;
 };
 
-Wallet.prototype.transferInto = function (wallet, amount) {
+Wallet.prototype.transferInto = function(wallet, amount) {
   console.log(
     `Transferring ${eurosFormatter.format(amount)} from ${
       this._name
@@ -29,15 +38,25 @@ Wallet.prototype.transferInto = function (wallet, amount) {
   wallet.deposit(withdrawnAmount);
 };
 
-Wallet.prototype.reportBalance = function () {
+Wallet.prototype.reportBalance = function() {
   console.log(
     `Name: ${this._name}, balance: ${eurosFormatter.format(this._cash)}`
   );
 };
 
-Wallet.prototype.getName = function () {
+Wallet.prototype.getName = function() {
   return this._name;
 };
+
+Wallet.prototype.resetDailyAllowance = function() {
+  this._dayTotalWithdrawals = 0;
+};
+
+Wallet.prototype.setDailyAllowance = function(newAllowance) {
+  this._dailyAllowance = newAllowance;
+};
+
+
 
 function main() {
   const walletJack = new Wallet('Jack', 100);
